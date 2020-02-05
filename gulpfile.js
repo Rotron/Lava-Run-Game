ee758a352f498728757e81699631149e6d1b007b
Federico Saccà <info@federicosacca.it>
2020-02-05 12:53:02 +0000

    Now it's working

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,15 @@
-// including plugins
-var gulp = require('gulp'),
-    uglify = require("gulp-uglify"),
-    concat = require("gulp-concat");
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifyJS = require('gulp-minify');
+
 
 var srcPath = ['./lib/**/*.js', './src/entities/*.js', './src/boot.js',  './src/load.js',  './src/credits.js', './src/play.js',   './src/menu.js', './src/main.js'];
 
-// default
 gulp.task('default', function() {
-
-    gulp.src('./**/*.js')
-        .pipe(concat('lava-run.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./build'));
-});
-
-
-// task
-gulp.task('uglify', function() {
-    gulp.src('./build/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build'));
-});
-
-
-// task
-gulp.task('concat', function() {
-    gulp.src(srcPath) // path to your files
-    .pipe(concat('concat.js')) // concat and name it "concat.js"
-    .pipe(gulp.dest('./build'));
+    return gulp.src(srcPath)
+        .pipe(minifyJS())
+        .pipe(concat('bundle.min.js'))
+        .pipe(uglify({ mangle: false }))
+        .pipe(gulp.dest('./build/'));
 });
